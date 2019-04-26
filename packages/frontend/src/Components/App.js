c51ad30493a7bef494381b7bb87584af5f5ddeda
Yoonu cho <yoonu78@naver.com>
2019-04-26 17:25:40 +0900

    수정: 실제 데이터받아서 출력하기 시도중

--- packages/frontend/src/Components/App.js
+++ packages/frontend/src/Components/App.js
@@ -1,129 +1,59 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import GlobalStyle from "./GlobalStyles";
+// import XMLParser from "react-xml-parser";
 import Box02 from "./Box02";
 
-const Container = styled.div`
-	padding: 50px;
-	padding-bottom: 100px;
-	display: grid;
-	grid-template-columns: repeat(auto-fill, 200px);
-	grid-gap: 20px;
-	grid-auto-rows: 300px;
-	justify-content: space-around;
-	// grid-template-rows: 260px 400px 400px;
-	& > div {
-		grid-column: span 1;
-		// &:nth-child(5),
-		// &:nth-child(6) {
-		// 	grid-column: span 1;
-		// }
-		// &:nth-child(8) {
-		// 	grid-column: span 4;
-		// }
-		// &:nth-child(14) {
-		// 	grid-column: span 4;
-		// }
-	}
-	@media screen and (max-width: 700px) {
-		grid-template-columns: 1fr;
-		grid-gap: 50px;
-		padding: 10px;
-		& > div {
-			grid-column: span 1 !important;
-		}
-	}
-`;
-
-const cats = [
-	{
-		name: "코숏",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904231404427.jpg"
-	},
-	{
-		name: "터키시 앙고라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904201604512.jpg"
-	},
-	{
-		name: "페르시안 친칠라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904181504135.jpg"
-	},
-	{
-		name: "코숏",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904231404427.jpg"
-	},
-	{
-		name: "터키시 앙고라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904201604512.jpg"
-	},
-	{
-		name: "페르시안 친칠라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904181504135.jpg"
-	},
-	{
-		name: "코숏",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904231404427.jpg"
-	},
-	{
-		name: "터키시 앙고라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904201604512.jpg"
-	},
-	{
-		name: "페르시안 친칠라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904181504135.jpg"
-	},
-	{
-		name: "터키시 앙고라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904201604512.jpg"
-	},
-	{
-		name: "페르시안 친칠라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904181504135.jpg"
-	},
-	{
-		name: "코숏",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904231404427.jpg"
-	},
-	{
-		name: "터키시 앙고라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904201604512.jpg"
-	},
-	{
-		name: "페르시안 친칠라",
-		bgPhoto:
-			"http://www.animal.go.kr/files/shelter/2019/04/201904181504135.jpg"
-	}
-];
-
 class App extends Component {
-	render() {
-		return (
-			<Container>
-				{cats.map(box02 => {
-					return (
-						<Box02
-							name={box02.name}
-							bgPhoto={box02.bgPhoto}
-							// bgPhoto="http://www.animal.go.kr/files/shelter/2019/04/201904231404427.jpg"
-						/>
-					);
-				})}
+	state = {};
 
-				<GlobalStyle />
-			</Container>
+	componentDidMount() {
+		this.getInfo();
+	}
+
+	renderInfo = () => {
+		const cats = this.state.cats.map(box02 => {
+			console.log(box02);
+			return (
+				<Box02
+					key={box02.id}
+					kindCd={box02.kindCd}
+					popfile={box02.popfile}
+				/>
+			);
+		});
+		return cats;
+	};
+
+	getInfo = async () => {
+		const cats = await this.callApi();
+		this.setState({
+			cats
+		});
+	};
+
+	callApi = () => {
+		return fetch(
+			"https://s3.ap-northeast-2.amazonaws.com/web1-html-internet-yu/cats-info04.json"
+			// "http://d1bj4sto4aozbg.cloudfront.net/cats-info04.json"
+			// "https://s3.ap-northeast-2.amazonaws.com/web1-html-internet-yu/cats-info04.json"
+
+		)
+			// .then(response => response.text())
+			// // .then(xmlText => console.log(xmlText))
+			// .then(response => (response.json()))
+			.then(response => (response.json()))
+			// .then(stringToXml =>
+			// 	new XMLParser().parseFromString(stringToXml)
+			// )
+			.then(json => json.data.cats)
+			.catch(err => console.log("error omg!"))
+	};
+
+	render() {
+		const { cats } = this.state;
+		return (
+			<div className={cats ? "App" : "App--loading"}>
+				{cats ? this.renderInfo() : "Loading"}
+			</div>
 		);
 	}
 }
