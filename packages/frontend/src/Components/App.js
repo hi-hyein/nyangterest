7ebd56c725c0bf2e03425ec89c4380bcd3a18b82
Yoonu cho <yoonu78@naver.com>
2019-05-03 02:25:29 +0900

    수정: UI라이브러리 적용후 코드 수정외

--- packages/frontend/src/Components/App.js
+++ packages/frontend/src/Components/App.js
@@ -1,5 +1,40 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyles";
 import List from "./List";
+
+// eslint-disable-next-line no-unused-vars
+const Container = styled.div`
+	padding: 50px;
+	padding-bottom: 100px;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, 200px);
+	grid-gap: 20px;
+	grid-auto-rows: 300px;
+	justify-content: space-around;
+	// grid-template-rows: 260px 400px 400px;
+	& > div {
+		grid-column: span 1;
+		// &:nth-child(5),
+		// &:nth-child(6) {
+		// 	grid-column: span 1;
+		// }
+		// &:nth-child(8) {
+		// 	grid-column: span 4;
+		// }
+		// &:nth-child(14) {
+		// 	grid-column: span 4;
+		// }
+	}
+	@media screen and (max-width: 700px) {
+		grid-template-columns: 1fr;
+		grid-gap: 50px;
+		padding: 10px;
+		& > div {
+			grid-column: span 1 !important;
+		}
+	}
+`;
 
 class App extends Component {
 	state = {};
@@ -17,6 +52,7 @@ class App extends Component {
 					kindCd={info.kindCd}
 					popfile={info.popfile}
 					happenDt={info.happenDt}
+					bgPhoto={info.bgPhoto}
 				/>
 			);
 		});
@@ -41,7 +77,10 @@ class App extends Component {
 		const { catsApi } = this.state;
 		console.log(catsApi);
 		return (
-			<div className="App">{catsApi ? this.renderInfo() : "nothing"}</div>
+			<Container>
+				{catsApi ? this.renderInfo() : "nothing"}
+				<GlobalStyle />
+			</Container>
 		);
 	}
 }
--- packages/frontend/src/Components/List.js
+++ packages/frontend/src/Components/List.js
@@ -1,15 +1,52 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const Box02 = ({ kindCd, popfile, happenDt }) => {
+const Container = styled.div`
+	position: relative;
+	background-image: linear-gradient(rgba(0, 0, 0, 0.28), rgba(0, 0, 0, 0.7)),
+		url(${props => props.bgPhoto});
+	background-size: cover;
+	border-radius: 8px;
+	box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.08);
+	display: flex;
+	flex-direction: column;
+	background-position: center center;
+	align-items: center;
+	justify-content: space-between;
+	padding: 20px;
+	border-bottom: 5px solid ${props => props.borderBottomColor};
+`;
+
+const Content = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+	color: white;
+	margin-top: 40px;
+	margin-bottom: 20px;
+`;
+
+const List = ({
+	kindCd,
+	popfile,
+	happenDt,
+	borderBottomColor = "#087264",
+	bgPhoto
+}) => {
 	return (
-		<div className="Box02">
-			<div>
-				<h1>{kindCd}</h1>
-				<p>{happenDt}</p>
-				<CatImage popfile={popfile} alt={kindCd} />
-			</div>
-		</div>
+		<Container borderBottomColor={borderBottomColor} bgPhoto={bgPhoto}>
+			<Content>
+				<div className="List">
+					<div>
+						<h1>{kindCd}</h1>
+						<p>{happenDt}</p>
+						<CatImage popfile={popfile} alt={kindCd} />
+					</div>
+				</div>
+			</Content>
+		</Container>
 	);
 };
 
@@ -17,10 +54,10 @@ const CatImage = ({ popfile, alt }) => {
 	return <img src={popfile} alt={alt} className="CatImage" />;
 };
 
-Box02.propTypes = {
+List.propTypes = {
 	kindCd: PropTypes.string,
 	popfifle: PropTypes.string,
-	happenDt: PropTypes.string
+	happenDt: PropTypes.number
 };
 
 CatImage.propTypes = {
@@ -28,4 +65,4 @@ CatImage.propTypes = {
 	alt: PropTypes.string
 };
 
-export default Box02;
+export default List;
