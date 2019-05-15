71a090136d841f59cd6fadec5393d59947640bae
henyy1004 <henyy1004@naver.com>
2019-05-15 20:03:48 +0900

    Feature/hi header (#11)
    * 헤더작업중
    
    * 헤더작업중

--- packages/frontend/src/Components/App.js
+++ packages/frontend/src/Components/App.js
@@ -1,13 +1,35 @@
 import React, { Component, Fragment } from "react";
 import GlobalStyle from "./GlobalStyles";
 import List from "./List";
+import Header from "./Header";
+import LoginPopup from "./LoginPopup";
 
 class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			userState: 'login',
+			loginPopup: false
+		}
+	}
+
+	// 로그인 팝업 열기
+	openLoginPopup = ()=>{
+		this.setState({
+			loginPopup: true
+		})
+	}
 	render() {
 		return (
 			<Fragment>
-				<List />
-				<GlobalStyle />
+				<div id="wrap">
+					{/* 헤더 */}
+					<Header userState={this.state.userState} onClick={this.openLoginPopup}/>
+					{/* 로그인 팝업 */}
+					{this.state.loginPopup && <LoginPopup/>}
+					<List />
+					<GlobalStyle />
+				</div>
 			</Fragment>
 		);
 	}
---
+++ packages/frontend/src/Components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+class Header extends React.Component {
+	render() {
+		const HeaderStyle = {
+			backgroundColor: 'skyblue'
+		}
+
+		return (
+			<div className="header" style={HeaderStyle}>
+				<h1>NYANGTEREST</h1>
+					{console.log(this)}
+					{/* 로그인 상태 */}
+					{this.props.userState === 'login' &&
+						<div className="button-area">
+							<button type="button" onClick={()=>this.props.onClick()}>LOGIN</button>
+							<button type="button">JOIN</button>
+						</div>
+					}
+					{/* 로그아웃 상태 */}
+					{this.props.userState === 'logout' &&
+						<div className="button-area">
+							<button type="button">MENU</button>
+						</div>
+					}
+			</div>
+		);
+	}
+}
+
+export default Header;
+
