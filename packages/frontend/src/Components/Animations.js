b353d552660d3e6ad524ecf8ba943de5f804afce
Yoonu cho <yoonu78@naver.com>
2019-07-27 03:43:28 +0900

    수정: 어드민 회원리스트 테이블로 UI변경

---
+++ packages/frontend/src/Components/Animations.js
@@ -0,0 +1,12 @@
+import { keyframes } from "styled-components";
+
+export const fadeInUp = keyframes`
+		from {  transform: translate3d(0,40px,0); opacity: 0; }
+		to   { transform: translate3d(0,0,0);opacity: 1; }
+`;
+
+
+
+
+
+
