d9c9221c28e92a7293e3e049e89e24501ccf1ef7
hyein7230 <hyein7230@naver.com>
2019-09-23 18:02:57 +0900

    md파일, 기타수정

--- packages/frontend/src/Stores/loginStore.js
+++ packages/frontend/src/Stores/loginStore.js
@@ -17,31 +17,4 @@ export default class loginStore {
 	changeUserId = (name)=>{
 		this.userId= name
 	}
-
-	// @action
-	// handleKeyPress = (e) => {
-	// 	if (e.key === "Enter") {
-	// 		console.log("test!")
-	// 		// e.preventDefault();
-	// 		const { pageNo, numOfRows, loadList } = this.root.listStore;
-	// 		const url = `/search/${numOfRows}/${pageNo}`;
-
-	// 		runInAction(() => {
-	// 			return loadList(), this.search = "";
-	// 		});
-	// 	}
-
-	// }
-
-	// @action
-	// handleSubmit = (e) => {
-	// 	e.preventDefault();
-	// 	const data = new FormData(e.target);
-
-	// 	fetch('/search/submit', {
-	// 		method: 'POST',
-	// 		body: data,
-	// 	});
-	// }
-
 }
