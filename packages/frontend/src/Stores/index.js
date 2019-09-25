08b0fa5097580d1c664a590c1100c6d81c5576d7
henyy1004 <henyy1004@naver.com>
2019-09-25 14:59:15 +0900

    Feature/login (#64)
    * 문서작업중
    
    * 로그인 작업중
    
    * 로그인 passport 작업중
    
    * mobx적용
    
    * md파일, 기타수정
    
    * session-file-store추가

--- packages/frontend/src/Stores/index.js
+++ packages/frontend/src/Stores/index.js
@@ -1,10 +1,12 @@
 import ListStore from "./listStore";
 import SearchStore from "./searchStore";
+import loginStore from "./loginStore";
 
 class RootStore {
 	constructor() {
 		this.listStore = new ListStore(this);
 		this.searchStore = new SearchStore(this);
+		this.loginStore = new loginStore(this);
 	}
 }
 
