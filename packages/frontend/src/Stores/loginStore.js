0de5efef750d90b59b689c1da92ead0829c580cb
hyein <nt99228@nhn.com>
2019-10-16 19:55:17 +0900

    로그아웃 기능추가

--- packages/frontend/src/Stores/loginStore.js
+++ packages/frontend/src/Stores/loginStore.js
@@ -2,7 +2,7 @@ import { observable, action } from "mobx";
 
 export default class loginStore {
 	@observable userId = "";
-	@observable userState = "logout";
+	@observable userState = "login";
 
 	constructor(root) {
 		this.root = root;
@@ -10,7 +10,12 @@ export default class loginStore {
 
 	@action
 	changeUserState = ()=>{
-		this.userState = "login"
+		if(this.userState === "logout"){
+			this.userState = "login"	
+		}else {
+			this.userState = "logout"
+		}
+		
 	}
 
 	@action
