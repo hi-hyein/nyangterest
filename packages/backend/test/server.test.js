74d5a3f780b0a7881129634908ab08466e9d0a59
Yoonu cho <yoonu78@naver.com>
2020-04-10 19:59:53 +0900

    수정: 유닛테스트 코드 작성

--- packages/backend/test/server.test.js
+++ packages/backend/test/server.test.js
@@ -1,36 +1,40 @@
+
 const request = require('supertest')
-const app = require('../server_ex')
+const app = require('../server')
 const fetch = require("node-fetch");
 
 describe('GET request parameters', () => {
 	// before('test', () => {
 
 	// })
-	const url = 'https://jsonplaceholder.typicode.com/posts/1'
 
-	const getData = async (url) => {
-		try {
-			const response = await fetch(url);
-			const json = await response.json();
-			return json;
+	const url = '/page/20200401/20200408/72/000116';
 
-		} catch (error) {
-			console.log(error);
-		}
-	}
+	const items = { "id": 1 }
 
 	test('works with async / await', async () => {
-		expect.assertions(1);
+
+		const getData = async (url) => {
+			try {
+				const response = await fetch(url);
+				const json = await response.json(items)
+				return json;
+
+			} catch (error) {
+				console.log(error);
+			}
+		}
 		const data = await getData(url);
-		expect(data.id).toBe(1)
-
-
+		expect(data).toEqual(
+			{ id: 1 }
+		);
 	})
 
-	test('async / await status code', async (done) => {
-		const response = await request(app).get("/page/:bgnde/:endde/:numOfRows/:kind");
+	test('async / await status code', async () => {
+		// const response = await request(app).get();
+		const response = await request(app).get(url);
 		expect(response.statusCode).toBe(200);
-		done()
+
 	})
 
 	test('test search/kind', (done) => {
