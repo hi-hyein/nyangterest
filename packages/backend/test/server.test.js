2b16a333d4548f2b0de3c02d19dd8ca622503f7b
Yoonu cho <yoonu78@naver.com>
2020-04-12 19:43:16 +0900

    수정: 유닛테스트 코드 작성

--- packages/backend/test/server.test.js
+++ packages/backend/test/server.test.js
@@ -2,39 +2,43 @@
 const request = require('supertest')
 const app = require('../server')
 const fetch = require("node-fetch");
+require('dotenv').config()
+
+const serviceKey = process.env.SERVICE_KEY;
+
+const api = 'http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc';
+
+const url = `${api}/abandonmentPublic?ServiceKey=${serviceKey}&_type=json&bgnde=20200405&endde=20200405&numOfRows=20&upkind=422400&kind=`;
 
 describe('GET request parameters', () => {
+
 	// before('test', () => {
 
 	// })
 
-	const url = '/page/20200401/20200408/72/000116';
-
-	const items = { "id": 1 }
 
 	test('works with async / await', async () => {
 
 		const getData = async (url) => {
 			try {
 				const response = await fetch(url);
-				const json = await response.json(items)
-				return json;
+				const json = await response.json();
+				const items = await json.response.body.items;
+				return items;
 
 			} catch (error) {
 				console.log(error);
 			}
 		}
 		const data = await getData(url);
-		expect(data).toEqual(
-			{ id: 1 }
-		);
+		expect.assertions(1);
+		expect(data.item[0].colorCd).toEqual("고등어");
 	})
 
 	test('async / await status code', async () => {
 		// const response = await request(app).get();
-		const response = await request(app).get(url);
+		const response = await request(app).get("/page/20200405/20200412/72/000116");
 		expect(response.statusCode).toBe(200);
-
 	})
 
 	test('test search/kind', (done) => {
