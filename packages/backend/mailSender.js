89713cccaf36d30888b38b40d6a3789d5234c9c0
henyy1004 <henyy1004@naver.com>
2020-05-31 17:37:12 +0900

    Feature/join mail (#201)
    * [회원가입] query table명 수정, nodemailer 옵션 수정
    
    * [로그인] query table명 수정
    
    * 비밀번호 입력시 #문자 허용
    
    * nodemailer 분리, member table명 수정
    
    Co-authored-by: hyein <junghi0327@gmail.com>

---
+++ packages/backend/mailSender.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer');
+
+// 메일 발송 객체
+const mailSender = {
+	// gmail발송
+	sendGmail : function(param){
+        const transporter = nodemailer.createTransport({
+            service: 'gmail'
+            ,prot : 587
+            ,host :'smtp.gmlail.com'
+            ,secure : false
+            ,requireTLS : true
+            , auth: {
+              user: 'nyangterest@gmail.com'
+              ,pass: 'hzdyfwgydlsalfkg'
+            }
+		});
+		
+        // 메일 옵션
+        const mailOptions = {
+			from: 'nyangterest@gmail.com',
+			to: param.toEmail, // 수신할 이메일
+			subject: param.subject, // 메일 제목
+			text: param.text // 메일 내용
+		};
+
+        // 메일 발송    
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            } else {
+            console.log('Email sent: ' + info.response);
+            }
+        });
+    }
+}
+
+module.exports = mailSender;
