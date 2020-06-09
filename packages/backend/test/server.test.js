aa40421b010400cfff6c102085538cc267b7284d
Yoonu cho <yoonu78@naver.com>
2020-06-09 19:26:54 +0900

    수정: Unit Test code 수정 및 api, filterArr() 분리

--- packages/backend/test/server.test.js
+++ packages/backend/test/server.test.js
@@ -1,347 +1,11 @@
-
 const request = require('supertest')
 const server = require('../server')
 
 describe("Nyangterest Unit test!", () => {
-	const per = 6;
-	const startDay = new Date(Date.now() + -1 * 24 * 3600 * 1000).toISOString().slice(0, 10).replace(/-/g, "");
-	const endDay = new Date().toISOString().slice(0, 10).replace(/-/g, "");
-	const str = "keyword"
-	const url = `/page/${startDay}/${endDay}/${per}/000116/${str}`;
 
-	const getData = async (url) => {
-		try {
-			const response = await request(server.app).get(url);
-			const items = await response.body.items;
-			return items;
-
-		} catch (error) {
-			console.log(error);
-		}
-	};
 
 	// Mock Object
-	const body_ex = {
-		"items":
-			[[
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200519,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "U",
-					"sexCd": "F",
-					"weight": "1(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "검정,갈색",
-					"happenDt": 20200519,
-					"kindCd": "[고양이] 기타",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200519,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2017(년생)",
-					"colorCd": "회색",
-					"happenDt": 20200519,
-					"kindCd": "[고양이] 러시안 블루",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.8(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200519,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200519,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "Y",
-					"sexCd": "F",
-					"weight": "3(Kg)"
-				}
-			],
-			[
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200518,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "U",
-					"sexCd": "F",
-					"weight": "1(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "검정,갈색",
-					"happenDt": 20200518,
-					"kindCd": "[고양이] 기타",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200518,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2017(년생)",
-					"colorCd": "회색",
-					"happenDt": 20200518,
-					"kindCd": "[고양이] 러시안 블루",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.8(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200518,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200518,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "Y",
-					"sexCd": "F",
-					"weight": "3(Kg)"
-				}
-			],
-			[
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200517,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "U",
-					"sexCd": "F",
-					"weight": "1(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "검정,갈색",
-					"happenDt": 20200517,
-					"kindCd": "[고양이] 기타",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200517,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2017(년생)",
-					"colorCd": "회색",
-					"happenDt": 20200517,
-					"kindCd": "[고양이] 러시안 블루",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.8(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200517,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200517,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "Y",
-					"sexCd": "F",
-					"weight": "3(Kg)"
-				}
-			],
-			[
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200516,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "U",
-					"sexCd": "F",
-					"weight": "1(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "검정,갈색",
-					"happenDt": 20200516,
-					"kindCd": "[고양이] 기타",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200516,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2017(년생)",
-					"colorCd": "회색",
-					"happenDt": 20200516,
-					"kindCd": "[고양이] 러시안 블루",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.8(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200516,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200516,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "Y",
-					"sexCd": "F",
-					"weight": "3(Kg)"
-				}
-			],
-			[
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200515,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "U",
-					"sexCd": "F",
-					"weight": "1(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "검정,갈색",
-					"happenDt": 20200515,
-					"kindCd": "[고양이] 기타",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200515,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2017(년생)",
-					"colorCd": "회색",
-					"happenDt": 20200515,
-					"kindCd": "[고양이] 러시안 블루",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.8(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200515,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200515,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "Y",
-					"sexCd": "F",
-					"weight": "3(Kg)"
-				}
-			],
-			[
-				{
-					"age": "2020(년생)",
-					"colorCd": "치즈노랑",
-					"happenDt": 20200514,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "U",
-					"sexCd": "F",
-					"weight": "1(Kg)"
-				},
-				{
-					"age": "2020(년생)",
-					"colorCd": "검정,갈색",
-					"happenDt": 20200514,
-					"kindCd": "[고양이] 기타",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2018(년생)",
-					"colorCd": "흰색",
-					"happenDt": 20200514,
-					"kindCd": "[고양이] 한국 고양이",
-					"neuterYn": "N",
-					"sexCd": "M",
-					"weight": "0.4(Kg)"
-				},
-				{
-					"age": "2017(년생)",
-					"colorCd": "회색",
-					"happenDt": 20200514,
-					"kindCd": "[고양이] 러시안 블루",
-					"neuterYn": "Y",
-					"sexCd": "M",
-					"weight": "0.8(Kg)"
-				}
-			]]
-	}
+
 	const body = {
 		"items":
 			[
@@ -654,71 +318,86 @@ describe("Nyangterest Unit test!", () => {
 			]
 	}
 
-	describe('GET request parameters test', () => {
+	const data = body.items;
 
-		const data = body.items;
+	describe('Call filterArr func async / await', () => {
 
-		test.only('Test function async/await', async () => {
+		test('Test colorCd function async/await', async () => {
 			const result = await server.filterArr(data, "흰색");
 			expect((result.length) > 0).toBeTruthy();
 			expect(result[0].colorCd).toEqual("흰색")
 
 		})
 
-		test('True and False test', () => {
-			expect((data[0][0]).age.length > 0).toBeTruthy();
+		test('Test sexCd function async/await', async () => {
+			const result = await server.filterArr(data, "암컷");
+			expect(result).toBeDefined()
+			expect(result[0].sexCd).toBe("F")
 		})
 
-		// 품종코드
-		test('kind ', () => {
-
-			for (let i = 0; i < data.length; i++) {
-				expect(data).toBeDefined()
-				expect(data).not.toBeNull();
-				expect(data[i][0].kindCd).toContain("[고양이] 한국 고양이");
-				expect((data[i][0]).length).not.toBe(0);
-			}
-
+		test('Test kindCd function async/await', async () => {
+			const result = await server.filterArr(data, "코리안숏헤어");
+			expect(result).toBeDefined()
+			expect(data).not.toBeNull();
+			expect(result[0].kindCd).toContain("[고양이] 한국 고양이")
 		})
 
-		// 예외처리 
-		test('async(error) / await', async () => {
-			const kindCd = Promise.resolve(data[0][1].kindCd)
+		test('Test age async(error) / await', async () => {
+			const result = await server.filterArr(data, "2018년생");
+			console.log(result)
+			const age = Promise.resolve(result[0].age)
 			const error = Promise.reject('error')
-			await expect(kindCd).toBeDefined()
-			await expect(kindCd).not.toBeNull();
-			await expect(kindCd).resolves.toContain("[고양이]");
+			await expect(age).toBeDefined()
+			await expect(age).not.toBeNull();
+			await expect(age).resolves.toContain("2018(년생");
 			await expect(error).rejects.toBe('error')
 
 		})
 
+	})
 
-		// 고양이가 포함되어있는지 확인  
-		test('TtoMatch default ', () => {
-			expect(data[0][0].length).not.toBe(0)
+	describe('API test ,async / await', () => {
+
+		const per = 6;
+		const startDay = new Date(Date.now() + -1 * 24 * 3600 * 1000).toISOString().slice(0, 10).replace(/-/g, "");
+		const endDay = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+		const str = "keyword"
+		const url = `/page/${startDay}/${endDay}/${per}/000116/${str}`;
+
+		test('Status code async / await ', async () => {
+			const response = await request(server.app).get(url);
+			expect(response.statusCode).toBe(200);
+		})
+
+
+		test('Default true or false', async () => {
+			const response = await request(server.app).get(url);
+			const body = response.body;
+			expect.assertions(2);
+			expect(data.length >= per).toBeTruthy();
+			expect(data.length < per).toBeFalsy();
+		})
+
+		test('ToMatch default ', async () => {
+			const response = await request(server.app).get(url);
+			const body = response.body;
+			expect(data.length).not.toBe(0)
 			for (let i = 0; i < data.length; i++) {
-				expect((data[i][0]).kindCd).toMatch(/고양이/)
-				expect((data[i][0])).toBeDefined()
-				expect((data[i][0])).not.toBeNull();
+				expect((data[i]).kindCd).toMatch(/고양이/)
+				expect((data[i])).toBeDefined()
+				expect((data[i])).not.toBeNull();
 			}
 
 		})
 
-		test('Default', () => {
-			expect.assertions(2);
-			expect(data[0].length <= per).toBeTruthy();
-			expect(data[0].length > per).toBeFalsy();
+		test('Input keyword(한글) with async / await', async () => {
+			const response = await request(server.app).get(`/page/20200405/20200412/72/000116/${encodeURI("치즈")}`);
+			const body = response.body;
+			expect.assertions(1);
+			expect(data[0].colorCd).toContain("치즈");
 		})
 
-		// test('Test search/kind', (done) => {
-		// 	request(app)
-		// 		.get('/search/kind')
-		// 		.set('Content-Type', 'application/json')
-		// 		.expect(200, done)
-		// })
 	})
 
-})
 
-
-
+})	
