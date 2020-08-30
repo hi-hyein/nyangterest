49f63355c7f094f4445d491a71e385409b80a6b4
hyein <junghi0327@gmail.com>
2020-08-30 23:18:00 +0900

    prettier

--- packages/backend/mailSender.js
+++ packages/backend/mailSender.js
@@ -1,45 +1,45 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const path = require("path");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
 // 환경설정 파일에서 Gmail계정정보 가져오기
-dotenv.config({ path: path.join(__dirname, './.env') })
+dotenv.config({ path: path.join(__dirname, "./.env") });
 const GMAILID = process.env.GMAILID;
 const GMAILPW = process.env.GMAILPW;
 
 // 메일 발송 객체
 const mailSender = {
-	// gmail발송
-	sendGmail : function(param){
+    // gmail발송
+    sendGmail: function (param, callback) {
         const transporter = nodemailer.createTransport({
-            service: 'gmail'
-            ,prot : 587
-            ,host :'smtp.gmlail.com'
-            ,secure : false
-            ,requireTLS : true
-            , auth: {
-              user: `${GMAILID}`
-              ,pass: `${GMAILPW}`
-            }
-		});
-		
+            service: "gmail",
+            prot: 587,
+            host: "smtp.gmlail.com",
+            secure: false,
+            requireTLS: true,
+            auth: {
+                user: `${GMAILID}`,
+                pass: `${GMAILPW}`,
+            },
+        });
+
         // 메일 옵션
         const mailOptions = {
-			from: 'nyangterest@gmail.com',
-			to: param.toEmail, // 수신할 이메일
-			subject: param.subject, // 메일 제목
-			text: param.text // 메일 내용
-		};
+            from: "nyangterest@gmail.com",
+            to: param.toEmail, // 수신할 이메일
+            subject: param.subject, // 메일 제목
+            text: param.text, // 메일 내용
+        };
 
-        // 메일 발송    
-        transporter.sendMail(mailOptions, function(error, info){
+        // 메일 발송
+        transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
-            console.log(error);
+                console.log(error);
             } else {
-            console.log('Email sent: ' + info.response);
+                console.log("Email sent: " + info.response);
             }
         });
-    }
-}
+    },
+};
 
 module.exports = mailSender;
