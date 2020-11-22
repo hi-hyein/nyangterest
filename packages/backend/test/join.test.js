0162b96fa989469bd96d3ebe6225171308d4662d
hyein <junghi0327@gmail.com>
2020-11-23 00:44:50 +0900

    #278 beforeAll, afterAll 추가

--- packages/backend/test/join.test.js
+++ packages/backend/test/join.test.js
@@ -1,30 +1,24 @@
-const request = require('supertest');
-const fs = require("fs");
-const moment = require("moment");
-const bcryptjs = require("bcryptjs");
-const dotenv = require("dotenv");
-const path = require("path");
 const _join = require('../join/_join');
 const test_db = require('../db/test_db');
 
-// 환경설정 파일에서 이메일 인증 Secret key 가져오기
-dotenv.config({ path: path.join(__dirname, "./.env") });
-const EMAIL_CERTIFY_KEY = process.env.EMAIL_CERTIFY_KEY;
-
+beforeAll(()=>{
+	test_db.createDatabase();
+})
 
 describe('사용자가 소셜 로그인에 성공', () => {
-	test('중복계정이 없다면 성공', async () => {
-		test_db.connect.connect();
-		test_db.createDB(test_db.connect);
-		// test_db.createTable('CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))');
-		// const request = {
-		// 	body: {
-		// 		"useremail": "nyangterest@test.com",
-		// 		"snsName": "google",
-		// 	}
-		// };
-		
-		// expect(_join.existUserEmail(request)).toEqual(false);
-		test_db.dropDB();
+	test('중복계정이 없다면 성공', () => {
+		// test_db.createTableMember();
+		// test_db.Addmember();
+		const request = {
+			"body": {
+				"useremail": "nyangterest@test.com",
+				"snsName": "google",
+			}
+		};
+		expect(_join.existUserEmail(request)).toBe(false);
 	})
+})
+
+afterAll(()=>{
+	test_db.dropDatabase();
 })
